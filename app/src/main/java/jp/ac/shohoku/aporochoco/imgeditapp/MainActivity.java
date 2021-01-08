12240bfcddd0667f2a9c24c9a5555c0c404d25a9
s19B707 <s19b707@shohoku.ac.jp>
2021-01-08 11:50:18 +0900

    Merge branch 'master' of C:\Users\s19b707\AndroidStudioProjects\ImgEditApp with conflicts.

--- app/src/main/java/jp/ac/shohoku/aporochoco/imgeditapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/aporochoco/imgeditapp/MainActivity.java
@@ -2,17 +2,27 @@ package jp.ac.shohoku.aporochoco.imgeditapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ここからコード作成
-
-
+        //ボタンが押された時の処理
+        ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
