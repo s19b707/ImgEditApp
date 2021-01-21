9f3c0c688c9fdac07d6f1bd99f737d11a4d2fee1
s19B707 <s19b707@shohoku.ac.jp>
2021-01-21 17:36:14 +0900

    起動してメニューボタンを押すと、画面が遷移して遷移先のGET IMAGEのボタンを押すとファイル選択の画面が表示されます。何かしら画像を選択するとその画像が表示されるようになりました。以上がアップデート内容です。ｂｙ愛猫を無くした悲しい子（ブラックジョーク）

--- app/src/main/java/jp/ac/shohoku/aporochoco/imgeditapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/aporochoco/imgeditapp/MainActivity.java
@@ -1,27 +1,36 @@
 package jp.ac.shohoku.aporochoco.imgeditapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ボタンが押された時の処理
-        ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
-        imageButton1.setOnClickListener(new OnClickListener() {
+
+         ImageButton button1 = findViewById(R.id.imageButton1);
+        ImageButton button2 = findViewById(R.id.imageButton2);
+
+
+        button1.setOnClickListener(new OnClickListener() {
+
+            //ボタンが押された時の処理
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);  //インテントの作成
+                startActivity(intent);                                 //画面遷移
+            }
+        });
+        button2.setOnClickListener(new OnClickListener() {
+
+            //ボタンが押された時の処理
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);  //インテントの作成
+                startActivity(intent);                                 //画面遷移
             }
         });
     }
